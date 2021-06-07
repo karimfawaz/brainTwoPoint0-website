74b99ef5253a927a1ec726216c58db8d386e0e9f
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2021-06-08 00:11:19 +0300

    Added Img to each Skill Card

--- components/Skills.js
+++ components/Skills.js
@@ -4,26 +4,31 @@ const skillsList = [
     {
         name: "Flutter",
         description: "Flutter is a framework used to create cross-platform mobile applications. Brain 2.0 uses Flutter to create Minimum Viable Products for companies with upcoming deadlines to create a working product in record time",
+        img: "flutter.png"
 
     },
-    {
-        name: "HTML, CSS, JavaScript",
-        description: "HTML, CSS, and JavaScript are the fundamentals of web development. Brain 2.0 makes use of these languages to create static web pages and landing pages."
 
-    },
     {
         name: "Python",
+        img: "python.png"
 
 
     },
     {
         name: "Java & Kotlin",
+        img: "kotlinjava.png"
 
     },
     {
         name: "React.js & Next.js",
+        img: "nextreact.png",
 
-    }
+    }, 
+    {
+        name: "HTML, CSS, JavaScript",
+        description: "HTML, CSS, and JavaScript are the fundamentals of web development. Brain 2.0 makes use of these languages to create static web pages and landing pages.",
+        img: "cssjshtml.png"
+    },
 ]
 const Skills = () => {
     return (
@@ -32,15 +37,13 @@ const Skills = () => {
             <section className={cardStyles.cardList}>
 
                 {skillsList.map((skillCard) => (<article className={cardStyles.card}>
+                    <img src={skillCard.img} height="150px" width="150px" className={cardStyles.skillImg} />
                     <header className={cardStyles.cardHeader}>
+
                         <h2>{skillCard.name}</h2>
                         <p>{skillCard.description}</p>
                     </header>
                 </article>))}
-
-
-
-
             </section>
         </div>
     )
