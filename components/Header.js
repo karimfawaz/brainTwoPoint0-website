c06fd99ecf5e3cc860d5cc97537750207c66ece8
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-09 01:47:06 +0300

    Changed Slogan and Fixed Typing Animation

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,15 @@ import headerStyles from '../styles/Header.module.css'
 
 
 const Header = () => {
+    
     return (
         <div className={headerStyles.header}>
             <img src="brainlogo256.png" className={headerStyles.companyLogo} ></img>
             <div className={headerStyles.nameSlogan}>
             <h1 className={headerStyles.companyName}> Brain 2.0</h1>
-            <h4 className={headerStyles.companySlogan}> Coding the Future</h4>
+            <h4 className={headerStyles.companySlogan}>Powered by Intellect, Driven by Values.</h4>
+
+    
 
             </div>
         </div>
