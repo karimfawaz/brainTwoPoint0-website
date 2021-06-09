169a35791425abb505f1229b37447a9692bd7720
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-09 18:31:36 +0300

    Changed Slogan

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <img src="brainlogo256.png" className={headerStyles.companyLogo} ></img>
             <div className={headerStyles.nameSlogan}>
             <h1 className={headerStyles.companyName}> Brain 2.0</h1>
-            <h4 className={headerStyles.companySlogan}>Powered by Intellect, Driven by Values.</h4>
+            <h4 className={headerStyles.companySlogan}>Driven by Values, Powered by Intellect.</h4>
 
     
 
