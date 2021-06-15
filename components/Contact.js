055c8d0d4520a546677cad7b0331d66be443492e
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-15 14:44:46 +0300

    Revert "Started Contact Form"
    This reverts commit 754502e68f63edba6c81cae0e3ace01fb90d05ee.

--- components/Contact.js
+++ components/Contact.js
@@ -1,36 +1,12 @@
 import contactStyles from '../styles/Contact.module.css'
 
-
-import emailjs from 'emailjs-com';
-
-
 const Contact = () => {
-    function sendEmail(e) {
-        e.preventDefault();
-
-        emailjs.sendForm('gmail', 'template_t444twk', e.target, 'user_Eqk0m1Vduvh2d2HcbRols')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-
-            e.target.reset();
-    }
-
     return (
-        <form className="contact-form" onSubmit={sendEmail}>
-            <input type="hidden" name="contact_number" />
-            <label>Name</label>
-            <input type="text" name="name" />
-            <label>Email</label>
-            <input type="email" name="email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
-        </form>
-    );
-
+        <div className={contactStyles.contactSection} id="contact">
+            <h4>Contact</h4>
+            
+        </div>
+    )
 }
 
 export default Contact
