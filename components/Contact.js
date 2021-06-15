754502e68f63edba6c81cae0e3ace01fb90d05ee
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-15 14:02:34 +0300

    Started Contact Form

--- components/Contact.js
+++ components/Contact.js
@@ -1,12 +1,36 @@
 import contactStyles from '../styles/Contact.module.css'
 
+
+import emailjs from 'emailjs-com';
+
+
 const Contact = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_t444twk', e.target, 'user_Eqk0m1Vduvh2d2HcbRols')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+            e.target.reset();
+    }
+
     return (
-        <div className={contactStyles.contactSection} id="contact">
-            <h4>Contact</h4>
-            
-        </div>
-    )
+        <form className="contact-form" onSubmit={sendEmail}>
+            <input type="hidden" name="contact_number" />
+            <label>Name</label>
+            <input type="text" name="name" />
+            <label>Email</label>
+            <input type="email" name="email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
+    );
+
 }
 
 export default Contact
