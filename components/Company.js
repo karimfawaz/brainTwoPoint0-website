a50e284b0a7a0813f3e384fbd3e4561606a33236
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2021-06-22 20:04:45 +0300

    Finished 1st Draft of Company name and slogan section

--- components/Company.js
+++ components/Company.js
@@ -2,7 +2,13 @@ import companyStyles from '../styles/Company.module.css'
 const Company = () => {
     return (
         <div className={companyStyles.companySection}>
-            <img className={companyStyles.logo} src="brainlogo256.png"/>
+            <h1 className={companyStyles.name}>BRAIN 2.0</h1>
+            <div className={companyStyles.container}>
+                <h2 className={companyStyles.sloganUp}>Driven by Values</h2>
+                <img className={companyStyles.logo} src="brainlogo256.png" />
+                <h2 className={companyStyles.sloganDown}>Powered by Intellect</h2>
+            
+            </div>
         </div>
     )
 }
