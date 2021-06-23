285f815c4859f34306bf33230731e2068d9d7168
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-23 15:04:30 +0300

    Attempting new way of centering absolute element

--- components/Company.js
+++ components/Company.js
@@ -7,7 +7,6 @@ const Company = () => {
                 <h2 className={companyStyles.sloganUp}>Driven by Values</h2>
                 <img className={companyStyles.logo} src="brainlogo256.png" />
                 <h2 className={companyStyles.sloganDown}>Powered by Intellect</h2>
-            
             </div>
         </div>
     )
