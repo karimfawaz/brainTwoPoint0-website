10c06a3c3bba46642e722b4aa9d7b3dd5f06038b
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-24 15:07:04 +0300

    Trying to add background image

--- components/Company.js
+++ components/Company.js
@@ -1,13 +1,12 @@
 import companyStyles from '../styles/Company.module.css'
+
 const Company = () => {
     return (
-        <div className={companyStyles.companySection}>
-            <h1 className={companyStyles.name}>BRAIN 2.0</h1>
-            <div className={companyStyles.container}>
-                <h2 className={companyStyles.sloganUp}>Driven by Values</h2>
+        <div className={companyStyles.companySection} >
+            <h2 className={companyStyles.sloganUp}>Driven by Values</h2>
                 <img className={companyStyles.logo} src="brainlogo256.png" />
-                <h2 className={companyStyles.sloganDown}>Powered by Intellect</h2>
-            </div>
+            
+            <h2 className={companyStyles.sloganDown}>Powered by Intellect</h2>
         </div>
     )
 }
