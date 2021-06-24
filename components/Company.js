f9aeb35b2cefbae13b092cd24297d3fb496db558
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-24 16:30:08 +0300

    Attempting new display

--- components/Company.js
+++ components/Company.js
@@ -1,11 +1,10 @@
 import companyStyles from '../styles/Company.module.css'
-
 const Company = () => {
     return (
-        <div className={companyStyles.companySection} >
+        <div className={companyStyles.companySection}>
             <h2 className={companyStyles.sloganUp}>Driven by Values</h2>
-                <img className={companyStyles.logo} src="brainlogo256.png" />
-            
+            <h1 className={companyStyles.name}>BRAIN 2.0</h1>
+            <img className={companyStyles.logo} src="brainlogo256.png" />
             <h2 className={companyStyles.sloganDown}>Powered by Intellect</h2>
         </div>
     )
