22b908e11979c426784adf72f3e5c440f1d99254
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2021-06-28 10:45:37 +0300

    Testing fade in animation

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,9 +1,26 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import React, { useEffect } from 'react';
+
+
 import { projectsList } from '../../list'
 
 import allprojectsStyles from '../../styles/Allprojects.module.css'
 const projects = () => {
+
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start('visible');
+        }
+        if (!inView) {
+            controls.start('hidden');
+        }
+    }, [controls, inView]);
     return (
         <div className={allprojectsStyles.projectsPage}>
             <Head>
@@ -25,12 +42,15 @@ const projects = () => {
                         <h4 className={projectStyles.text}>Coming Soon...</h4> :
                         projectsList.map((project) => (
 
-                            <Link key={project.name} href={"/projects/" + project.id}>
-                                <div className={allprojectsStyles.projectCard}>
+                            <Link key={project.id} href={"/projects/" + project.id}>
+                                <motion.div ref={ref} initial="hidden" animate={controls} variants={{
+                                    hidden: { x: 30, opacity: 0 },
+                                    visible: { x: 0, opacity: 1, transition: { delay: .5 } }
+                                }} className={allprojectsStyles.projectCard}>
                                     <img src={project.logo} className={allprojectsStyles.projectLogo} />
                                     <h3 className={allprojectsStyles.projectName}>{project.name}</h3>
                                     <p className={allprojectsStyles.projectShortdesc}>{project.shortDesc}</p>
-                                </div>
+                                </motion.div>
                             </Link>
                         ))}
             </div>
