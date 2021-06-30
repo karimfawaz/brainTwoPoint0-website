230bd71909b7b415b9f5c743bcad139ca3f6273e
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-30 16:30:19 +0300

    Updated Mobile Font Weight for project date and added animation for page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { motion } from 'framer-motion'
+import Router from 'next/dist/next-server/lib/router/router'
+function MyApp({ Component, pageProps, router }) {
+  return <motion.div key={router.route}
+    initial="pageInitial"
+    animate="pageAnimate"
+    variants={{
+      pageInitial: { opacity: 0, },
+      pageAnimate: {
+        opacity: 1,
+      }
+    }}>
+    <Component {...pageProps} />
+  </motion.div>
 }
 
 export default MyApp
