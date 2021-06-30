2f2156626e1d46223fe6672f2a5312c72791e2fb
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-06-30 17:35:11 +0300

    Finished Pikasso First draft

--- list.js
+++ list.js
@@ -17,7 +17,7 @@ export const projectsList = [
         logo: "/oneinstock.png",
         shortDesc: "A new way to shop online",
         text: "In 2019, Brain 2.0 launched its first E-Commerce Web-Store. OneInStock's purpose was making the customer's shopping experience as enjoyable as possible on a fluid interface using a custom-built e-commerce website: www.oneinstock.me (currently not available). The project was intended to impact the Lebanese market. In Lebanon, most online stores charge a very high commission for their stock. Brain 2.0 wanted to make the e-commerce sector a more competitive field by offering limited edition items on a high-end website for a very cheap price which would force other stores to offer better prices. We also offered free gifts with each order along with a handwritten thank you letter to build a connection with our customers. The store only had 1 item in stock which helped the user make faster decisions. On several occasions, we had two or more customers enquiring about the same item and because some of the customers missed out on one item they would buy another one very quickly to not miss out again. Due to unfortunate circumstances, Lebanon started its worst economic crisis in October 2019, 1 month after the website's launch. In January 2020, Lebanese banks blocked transactions using foreign currencies which meant we could not fund the hosting of OneInStock.me and had to revert to an Instagram-based store. From October until December 2019, we were recruiting Instagram-based stores to give them a free platform to advertise their products on OneInStock.me, we had Keto food stores, customized jewlery, customized phone cases, and customized socks ready to join the OneInStock.me ecosystem but due to the Lebanese banks' measures, the operation was halted. OneInStock still operates as an Instagram Store. When the economic situation improves in Lebanon we would look forward to resuming our platform project in Lebanon and hopefully expand our reach around the world.",
-        screenshots: ["/oissc1.jpg","/oiscsc2.jpg","/oiscsc3.jpg"],
+        screenshots: ["/oissc1.jpg", "/oiscsc2.jpg", "/oiscsc3.jpg"],
         links: []
 
 
@@ -28,7 +28,7 @@ export const projectsList = [
         date: "August 2020",
         logo: "/pikasso.png",
         shortDesc: "Helping small Businesses one app at a time",
-        text: "",
+        text: "2020 was one of the hardest years for Lebanon. Along with the worldwide pandemic, Lebanon went through an economic crisis which lead to unforseen gas, water, food, and electricity shortages. As an attempt to help small businesses, Brain 2.0 had decided to offer free mobile applications. Starting with a barbershop we've known for a long time, we have created a basic app that gives you the location of the barbershop, some contact information, and lets you book appointments. The project was supposed to be lauched during the first week of August. On August 4th, the Beirut Blast occured and halted all of our projects. We did launch the project a few weeks later but the initiative to financially help small businesses was postponed.",
         screenshots: ["/pikassoappointsc.png", "/pikassohomesc.png", "/pikassoalbumsc.png"],
         links: []
     },
