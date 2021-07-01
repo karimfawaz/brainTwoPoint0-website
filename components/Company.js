11f934ee21c5d473711d7974ecf919fec8eb1595
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-07-01 18:52:41 +0300

    Updated Animation delays

--- components/Company.js
+++ components/Company.js
@@ -24,22 +24,22 @@ const Company = () => {
         }} className={companyStyles.companySection}>
             <motion.h2 ref={ref} initial="hidden" animate={controls} variants={{
                 hidden: { x: -400, opacity: 0 },
-                visible: { x: 0, opacity: 1, transition: { delay: .5 } }
+                visible: { x: 0, opacity: 1, transition: { delay: 1 } }
             }} className={companyStyles.sloganUp}>Driven by Values</motion.h2>
             <div className={companyStyles.container}>
                 <motion.h1 ref={ref} initial="hidden" animate={controls} variants={{
                 hidden: { rotate: 0, opacity: 0 },
-                visible: { rotate: [0,2,-3,0], opacity: 1, transition: { delay: .5 } }
+                visible: { rotate: [0,2,-3,0], opacity: 1, transition: { delay: 1.5 } }
             }} className={companyStyles.name}>BRAIN 2.</motion.h1>
                 <motion.img ref={ref} initial="hidden" animate={controls} variants={{
                     hidden: { scale: 0 },
-                    visible: { scale: 1, transition: { delay: .5 } }
+                    visible: { scale: 1, transition: { delay: 2 } }
                 }}
                     className={companyStyles.logo} src="brain256.png" />
             </div>
             <motion.h2 ref={ref} initial="hidden" animate={controls} variants={{
                 hidden: { x: 400, opacity: 0 },
-                visible: { x: 0, opacity: 1, transition: { delay: .5 } }
+                visible: { x: 0, opacity: 1, transition: { delay: 2.5 } }
             }} className={companyStyles.sloganDown}>Powered by Intellect</motion.h2>
         </motion.div>
     )
