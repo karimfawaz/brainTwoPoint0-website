80b8b27049757b5b38f57dca3c9c9fb81efc3bef
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-15 01:05:20 +0000

    refactor form

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -5,7 +5,7 @@ export default function Form() {
         <form className="w-3/4 md:w-3/5" data-netlify="true" name="contact" method="POST" action="/">
             <input type="hidden" name="form-name" value="contact" />
             <div className="">
-                <div className="">
+                <div className="mb-2 md:mb-4">
                     <label htmlFor="firstName" className="text-[var(--light)] font-light ">
                         First Name:
                     </label>
@@ -13,15 +13,15 @@ export default function Form() {
                         required
                         type="text"
                         name="firstName"
-                        className="w-full bg-white rounded-md py-2 pl-4 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]"
+                        className="w-full bg-white rounded-md p-1 md:p-2 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]"
                     />
                 </div>
 
-                <div className="">
+                <div className="mb-2 md:mb-4">
                     <label htmlFor="lastName" className="text-[var(--light)] font-light ">
                         Last Name:
                     </label>
-                    <input required type="text" name="lastName" className="w-full bg-white rounded-md py-2 pl-4 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]" />
+                    <input required type="text" name="lastName" className="w-full bg-white rounded-md p-1 md:p-2 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]" />
                 </div>
             </div>
 
@@ -29,29 +29,29 @@ export default function Form() {
 
             <div className="">
 
-                <div className="">
+                <div className="mb-2 md:mb-4">
                     <label htmlFor="companyName" className="text-[var(--light)] font-light ">
                         Company Name:
                     </label>
-                    <input required type="text" name="companyName" className="w-full bg-white rounded-md py-2 pl-4 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]" />
+                    <input required type="text" name="companyName" className="w-full bg-white rounded-md p-1 md:p-2 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]" />
                 </div>
 
 
-                <div className=" ">
+                <div className="hidden md:block mb-2 md:mb-4">
                     <label htmlFor="companyWebsite" className="text-[var(--light)] font-light ">
                         Company Website (optional):
                     </label>
                     <input
                         type="url"
                         name="companyWebsite"
-                        className="w-full bg-white rounded-md py-2 pl-4 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]"
+                        className="w-full bg-white rounded-md p-1 md:p-2 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]"
                     />
                 </div>
             </div>
 
 
             <div className="">
-                <div className="">
+                <div className="mb-2 md:mb-4">
                     <label htmlFor="email" className="text-[var(--light)] font-light ">
                         E-Mail Address:
                     </label>
@@ -59,27 +59,27 @@ export default function Form() {
                         required
                         type="email"
                         name="email"
-                        className="w-full bg-white rounded-md py-2 pl-4 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]"
+                        className="w-full bg-white rounded-md p-1 md:p-2 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]"
                     />
                 </div>
 
 
-                <div className="">
+                <div className="hidden md:block mb-2 md:mb-4">
                     <label htmlFor="mobile" className="text-[var(--light)] font-light ">
                         Mobile Number (optional):
                     </label>
-                    <input type="text" name="mobile" className="w-full bg-white rounded-md py-2 pl-4 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]" />
+                    <input type="text" name="mobile" className="w-full bg-white rounded-md p-1 md:p-2 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)]" />
                 </div>
             </div>
 
-            <div className="">
+            <div className="mb-2 md:mb-4">
                 <label htmlFor="helpType" className="text-[var(--light)] font-light ">
                     How can we help?
                 </label>
                 <select
                     required
                     name="helpType"
-                    className="w-full h-10 bg-white rounded-md py-2 pl-4 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)] "
+                    className="w-full h-10 bg-white rounded-md p-1 md:p-2 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)] "
                 >
                     <option value="General & Feedback">General & Feedback</option>
                     {/* <option value="Freelance2.0">Freelance 2.0</option> */}
@@ -94,7 +94,7 @@ export default function Form() {
             <textarea
                 name="tellUsMore"
                 rows={2}
-                className="w-full bg-white rounded-md py-2 pl-4 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)] resize-vertical" />
+                className="w-full bg-white rounded-md p-1 md:p-2 focus:outline-none focus:ring-1 ring-[var(--teal)] font-light text-[var(--black)] resize-vertical" />
             <button type="submit" className="px-10 mt-6 py-2 bg-white text-[var(--black)] rounded-md text-lg border-[var(--teal)] border-2 hover:bg-[var(--teal)] hover:text-[var(--white)] transition-all duration-300 ease-in-out] w-full">
                 Submit
             </button>
