67981ab547306b711142b9ea4e030ea8adb75976
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-16 21:48:05 +0000

    fix navbar mobile sizing for social icons

--- src/app/common/navbar.tsx
+++ src/app/common/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="absolute top-0 w-screen flex p-2 md:p-6 justify-between items-center">
       <img src="/B2.0-icon.svg" alt="Brain 2.0 Icon" className="md:w-20 md:h-20 h-10 w-10" />
-      <div className="flex md:w-44 w-1/3 justify-evenly">
+      <div className="flex md:w-44 w-1/5 justify-between">
         <a href="https://www.instagram.com/braintwopoint0/"><img src="/insta.svg" alt="Instagram Icon" className="md:w-16 md:h-16 h-8 w-8" /></a>
         <a href="https://www.linkedin.com/company/98897834"><img src="/linkedin.svg" alt="LinkedIn Icon" className="md:w-16 md:h-16 h-8 w-8" /></a>
       </div>
