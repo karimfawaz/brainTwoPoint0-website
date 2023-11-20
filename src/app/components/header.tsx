8c9e8b6e741cf01e187acd9c35f1d03485f1e090
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-20 17:10:32 +0000

    switch to using Next.js Image

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,6 +2,7 @@
 import React, { useRef } from 'react';
 import { FaChevronDown } from 'react-icons/fa';
 import Navbar from '../common/navbar';
+import Image from 'next/image';
 
 export default function Header() {
     const nextSectionRef = useRef<HTMLDivElement | null>(null);
@@ -17,7 +18,7 @@ export default function Header() {
         <>
             <section className="relative h-full w-full snap-center flex flex-col items-center justify-center">
                 <Navbar />
-                <img src="/B2.0-textlogo.png" className="md:w-1/2 w-4/5" alt="Logo" />
+                <Image height={1000} width={2000} src="/B2.0-textlogo.png" className="md:w-1/2 w-4/5" alt="Logo" />
                 <p className="text-[var(--light)] mt-4 text-sm md:text-5xl">DRIVEN BY VALUES, POWERED BY INTELLECT</p>
                 <div className="absolute bottom-5 animate-bounce" onClick={scrollToNextSection}>
                     <FaChevronDown className="text-[var(--light)] text-3xl md:text-6xl" />
