66068631147f949957fed21aed259b909819e645
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-20 01:57:50 +0000

    (hopefully) fix page sizing

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -4,11 +4,11 @@ import Form from './form'
 export default function Contact() {
 
     return (
-        <section className="snap-center flex flex-row justify-start items-center ">
+        <section className="snap-center flex flex-row justify-start items-center overflow-scroll no-scrollbar">
             <div className="flex flex-col justify-center items-center h-full w-1/5 md:w-40">
                 <h1 className="font-bold text-7xl md:text-9xl text-[var(--teal)] rotate-90 ">CONTACT<span className="font-[100] inter-font">US</span></h1>
             </div>
-            <div className="w-full flex flex-col items-center">
+            <div className="w-full flex flex-col items-center justify-center md:p-12 min-h-screen">
                 <h1 className="text-[var(--light)] md:text-6xl text-3xl hidden md:block md:mb-6">LET US KNOW</h1>
                 <Form />
             </div>
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+"use client"
+import React, { useRef } from 'react';
 import { FaChevronDown } from 'react-icons/fa';
 import Navbar from '../common/navbar';
 
 export default function Header() {
+    const nextSectionRef = useRef<HTMLDivElement | null>(null);
+
+    const scrollToNextSection = () => {
+        if (nextSectionRef.current) {
+            nextSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+        nextSectionRef.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
-        <section className="relative h-full w-full snap-center flex flex-col items-center justify-center">
-            <Navbar />
-            <img src="/B2.0-textlogo.png" className="md:w-1/2 w-4/5" alt="Logo" />
-            <p className="text-[var(--light)] mt-4 text-sm md:text-5xl">DRIVEN BY VALUES, POWERED BY INTELLECT</p>
-            <div className="absolute bottom-5 animate-bounce">
-                <FaChevronDown className="text-[var(--light)] text-3xl md:text-6xl" />
-            </div>
-        </section>
-    )
+        <>
+            <section className="relative h-full w-full snap-center flex flex-col items-center justify-center">
+                <Navbar />
+                <img src="/B2.0-textlogo.png" className="md:w-1/2 w-4/5" alt="Logo" />
+                <p className="text-[var(--light)] mt-4 text-sm md:text-5xl">DRIVEN BY VALUES, POWERED BY INTELLECT</p>
+                <div className="absolute bottom-5 animate-bounce" onClick={scrollToNextSection}>
+                    <FaChevronDown className="text-[var(--light)] text-3xl md:text-6xl" />
+                </div>
+            </section>
+            <div ref={nextSectionRef}></div>
+
+        </>
+    );
 }
