85105d4d57c121cd078177270553b91e8036b322
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-20 17:15:11 +0000

    align slogan to centre for responsiveness consistency

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
             <section className="relative h-full w-full snap-center flex flex-col items-center justify-center">
                 <Navbar />
                 <Image height={1000} width={2000} src="/B2.0-textlogo.png" className="md:w-1/2 w-4/5" alt="Logo" />
-                <p className="text-[var(--light)] mt-4 text-sm md:text-5xl">DRIVEN BY VALUES, POWERED BY INTELLECT</p>
+                <p className="text-[var(--light)] mt-4 text-sm text-center md:text-5xl">DRIVEN BY VALUES, POWERED BY INTELLECT</p>
                 <div className="absolute bottom-5 animate-bounce" onClick={scrollToNextSection}>
                     <FaChevronDown className="text-[var(--light)] text-3xl md:text-6xl" />
                 </div>
