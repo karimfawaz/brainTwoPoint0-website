7fca90a974456d76292c9b2782c323a5dfaf7e5f
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-09-12 00:23:50 +0100

    edit PLAYBACK team

--- src/app/playback/team.tsx
+++ src/app/playback/team.tsx
@@ -9,7 +9,7 @@ export default function PLAYBACKTeam() {
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">The project&lsquo;s backbone, the <span className='font-bold'>TEAM</span>, is working hard to deliver the project as quickly as possible and enable you to start elevating your experience by curating mutually beneficial deals on an international scale.</p>
                 </div>
                 <div className="flex flex-wrap -m-2">
-                    <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
+                    <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
                         <div className="h-full flex items-center border p-4 rounded-lg border-[var(--dark-grey)]">
                             <img alt="team" className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/team/karim.jpeg" />
                             <div className="flex-grow">
@@ -19,30 +19,21 @@ export default function PLAYBACKTeam() {
                         </div>
                     </div>
 
-                    <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
+                    <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
                         <div className="h-full flex items-center border p-4 rounded-lg border-[var(--dark-grey)]">
-                            <img alt="team" className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/team/yuval.jpeg" />
+                            <img alt="team" className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/team/abbas.jpeg" />
                             <div className="flex-grow">
-                                <h2 className=" font-bold">Yuval Cole</h2>
-                                <p>Operations Director - España Ambassador</p>
+                                <h2 className=" font-bold">Abbas Kazmi</h2>
+                                <p>Partner</p>
                             </div>
                         </div>
                     </div>
-                    <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
+                    <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
                         <div className="h-full flex items-center border p-4 rounded-lg border-[var(--dark-grey)]">
-                            <img alt="team" className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/team/louis.jpeg" />
+                            <img alt="team" className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/team/jeff.jpg" />
                             <div className="flex-grow">
-                                <h2 className=" font-bold">Louis Gualda</h2>
-                                <p>Operations Director - France Ambassador</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
-                        <div className="h-full flex items-center border p-4 rounded-lg border-[var(--dark-grey)]">
-                            <img alt="team" className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/team/tommaso.jpeg" />
-                            <div className="flex-grow">
-                                <h2 className=" font-bold">Tommaso di Gregorio</h2>
-                                <p>Customer Acquisition Director - Italy Ambassador </p>
+                                <h2 className=" font-bold">Jeff Barnes</h2>
+                                <p>Finance Director</p>
                             </div>
                         </div>
                     </div>
